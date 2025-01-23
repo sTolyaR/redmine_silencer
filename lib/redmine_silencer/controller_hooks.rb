782c9d848eafdbdbe04d8cf46494c230c868556d
runtsov_aa <anatoliy.runtsov@gmail.com>
2025-01-23 12:36:55 +0300

    Fix silent mode for Rest API.
    - Fix disabling global switch on user authentication;
    - Fix suppressing mail notifications when global switch is on and change is started from Rest API (without View layer).

--- lib/redmine_silencer/controller_hooks.rb
+++ lib/redmine_silencer/controller_hooks.rb
@@ -9,15 +9,10 @@ module RedmineSilencer
       update_journal_notify(context[:params], context[:issue].current_journal)
     end
 
-    def controller_account_success_authentication_after(context)
-      context[:user].pref[:silencer_global_on] = false 
-      context[:user].pref.save
-    end
-
     private
 
     def update_journal_notify(params, journal)
-      if journal && params && params[:suppress_mail] == '1'
+      if journal && params && (params[:suppress_mail] == '1' || User.current.pref[:silencer_global_on])
         if (User.current.allowed_to?(:suppress_mail_issue_switch, journal.project) ||
             User.current.allowed_to?(:suppress_mail_global_switch, nil, :global => true))
           journal.notify = false
